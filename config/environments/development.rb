574abf015d7d808f9bdc95ff7fc8bf90db1398b0
MuzamilHussainHaideri <haiderimuzamil@gmail.com>
2022-03-24 18:27:34 +0500

    user authentication added + basic things setup

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -53,9 +53,6 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = { :address => '127.0.0.1', :port => 1025 }
-  config.action_mailer.raise_delivery_errors = false
 
   # Raises error for missing translations.
   # config.action_view.raise_on_missing_translations = true
@@ -63,4 +60,11 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = { :address => '127.0.0.1', :port => 1025 }
+  config.action_mailer.raise_delivery_errors = false
+
+
 end
